a77bb0f4122580f279623592eb92f26959ace48a
busyVision <>
2018-04-13 20:51:18 +0900

    Fix dimensions, add session

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from random import shuffle
 
 
 def toArray(binString):
-	return np.array([int(i) for i in binString])
+	# Wrap the resulting ints in an array with length of 1 to make the inputs 3-dimensional.
+	return np.array([[int(i)] for i in binString])
 
 
 # https://monik.in/a-noobs-guide-to-implementing-rnn-lstm-using-tensorflow/
 # Naming conventions:
 # (name)_d or t for raw data or tensors
 def main():
-	train_batch_size = 2 ** 20 // 4
-	test_batch_size = 2 ** 20 // 4
+	temp_dividend = 10
+	train_batch_size = 2 ** 20 // temp_dividend
+	test_batch_size = 2 ** 20 // temp_dividend
 	sequence_length = 20
 	num_hidden = 16
+	input_dim = 1
 	data_type = tf.float32
 
 	all_possible = ['{0:020b}'.format(i) for i in range(2 ** 20)] # Generate all 20-char long sequences of 0s and 1s
 	shuffle(all_possible)
 	print("dataset generated")
 
-	# Input data dimensions : [batch size, sequence_length]
+	# Input data dimensions : [batch size, sequence_length, input_dim]
 	train_input_d = [toArray(i) for i in all_possible[:train_batch_size]] # Take test_batch_size of that randomly as training data
 	train_expected_d = [np.sum(i) for i in train_input_d]
 	print("train set generated")
@@ -31,12 +34,19 @@ def main():
 	print("test set generated")
 
 	# Build the model
-	train_input_t = tf.placeholder(tf.float32, (None, sequence_length))
-	train_expected_t = tf.one_hot(train_expected_d, sequence_length + 1)
-	test_input_t = tf.placeholder(tf.float32, (None, sequence_length))
+	train_input_t = tf.placeholder(tf.float32, (None, sequence_length, 1))
+	train_expected_t = tf.one_hot(train_expected_d, sequence_length + 1) # TODO: separate data insertion from model building
+	test_input_t = tf.placeholder(tf.float32, (None, sequence_length, 1))
 	test_expected_t = tf.one_hot(test_expected_d, sequence_length + 1)
-	cell_t = tf.nn.rnn_cell.GRUCell(num_hidden)
+	cell_t = tf.nn.rnn_cell.LSTMCell(num_hidden)
 	output_t, state_t = tf.nn.dynamic_rnn(cell_t, train_input_t, dtype=data_type)
+
+	# Run the model
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		sess.run(output_t, feed_dict={train_input_t: train_input_d, test_input_t: test_input_d})
+		# Export to Tensorboard
+		tf.summary.FileWriter("/board").add_graph(sess.graph)
 
 
 if __name__ == "__main__":
